c0ba976d10f058a7db376c907925621a97aded40
Ajay A <ajaycalicut17@gmail.com>
2026-08-23 16:21:16 +0530

    login

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,19 +1,11 @@
-import { test, expect } from '@playwright/test';
+import { test as setup, expect } from '@playwright/test';
+import path from 'path';
 import { faker } from '@faker-js/faker';
 
-test.use({ storageState: { cookies: [], origins: [] } });
+const authFile = path.join(__dirname, '../.auth/user.json');
 
-test('login with invalid credentials', async ({ page }) => {
-  await page.goto('/');
-  await page.getByRole('textbox', { name: 'Email' }).fill('invalid@example.com');
-  await page.getByRole('textbox', { name: 'Password' }).fill('invalidpassword');
-  await page.getByRole('button', { name: 'Log in' }).click();
-
-  await expect(page).toHaveURL('/');
-  await expect(page.getByRole('paragraph')).toContainText('The provided credentials do not match our records.');
-});
-
-test('login', async ({ page }) => {
+setup('authenticate', async ({ page }) => {
+  // Start of authentication.
   const fullName = faker.person.fullName();
   const email = faker.internet.email();
   const password = faker.internet.password();
@@ -24,14 +16,14 @@ test('login', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Password', exact: true }).fill(password);
   await page.getByRole('textbox', { name: 'Confirm Password' }).fill(password);
   await page.getByRole('button', { name: 'Register' }).click();
-  await expect(page).toHaveURL('/');
 
   await page.goto('/');
   await page.getByRole('textbox', { name: 'Email' }).fill(email);
   await page.getByRole('textbox', { name: 'Password' }).fill(password);
   await page.getByRole('button', { name: 'Log in' }).click();
 
-  await expect(page).toHaveURL('/dashboard');
   await expect(page.getByRole('button', { name: fullName })).toBeVisible();
-  await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
+  // End of authentication.
+
+  await page.context().storageState({ path: authFile });
 });
--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect } from '@playwright/test';
 import { faker } from '@faker-js/faker';
 
+test.use({ storageState: { cookies: [], origins: [] } });
+
 test('register', async ({ page }) => {
   const fullName = faker.person.fullName();
   const email = faker.internet.email();
